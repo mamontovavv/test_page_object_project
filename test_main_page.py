10c81675db1e9f9748aeebe2d38b21a506cf069f
mamontovavv <69858117+mamontovavv@users.noreply.github.com>
2022-07-05 12:51:52 +0400

    Add login page locators

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,13 +12,15 @@ def go_to_login_page(browser):
     login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
