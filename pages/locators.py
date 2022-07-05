b7a526330c3f808c18c4756cc646e27517f01ea4
mamontovavv <69858117+mamontovavv@users.noreply.github.com>
2022-07-05 17:33:17 +0400

    add negative tests and move some methods to base page

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
@@ -14,3 +17,4 @@ class ProductPageLocators():
     PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
     MESSAGE_NAME = (By.CSS_SELECTOR, "div.alertinner  strong")
     MESSAGE_PRICE =  (By.CSS_SELECTOR, 'div.alertinner p strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(2)")
--- pages/product_page.py
+++ pages/product_page.py
@@ -15,4 +15,12 @@ class ProductPage(BasePage):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
         basket_price = self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE)
         assert product_price.text == basket_price.text, "The product price does not match"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+"Success message is presented, but should not be"
+
+    def should_be_disapeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE),\
+"Should be disappeared, but still is presented"
         
