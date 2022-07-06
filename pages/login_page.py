7a62726e294f8ce42dfde3d8a8b0e86f48374c37
mamontovavv <69858117+mamontovavv@users.noreply.github.com>
2022-07-06 18:40:34 +0400

    add mark for reviewing tests and delete extra comments

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,21 +7,16 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес      
+    def should_be_login_url(self):  
         assert "/login" in self.browser.current_url, "login is absent in current url"
 
-
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
 
     def register_new_user(self, email, password):
-        # принимает две строки и регистрирует пользователя. Реализуйте его, описав соответствующие элементы страницы.
           email_input = self.browser.find_element(*LoginPageLocators.EMAIL_INPUT)
           email_input.send_keys(email)
           password_input = self.browser.find_element(*LoginPageLocators.PASSWORD_INPUT)
